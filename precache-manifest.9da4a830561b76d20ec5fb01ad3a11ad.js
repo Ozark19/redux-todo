ab5677f145b9946032527a02632013f37936e56b
Ozark19 <55114864+Ozark19@users.noreply.github.com>
2020-03-16 15:54:34 -0300

    Updates

---
+++ precache-manifest.9da4a830561b76d20ec5fb01ad3a11ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1260c899d080c9b51a3cca2cf0425e9e",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "65626cacb07ab2bdbb96",
+    "url": "/redux-todo/static/css/main.6c7d706d.chunk.css"
+  },
+  {
+    "revision": "5fb494fa597d7d0d44ec",
+    "url": "/redux-todo/static/js/2.7b97c418.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/redux-todo/static/js/2.7b97c418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65626cacb07ab2bdbb96",
+    "url": "/redux-todo/static/js/main.dfcf35cc.chunk.js"
+  },
+  {
+    "revision": "6b25d187ec2ce6b8cda6",
+    "url": "/redux-todo/static/js/runtime-main.8ed9a3ef.js"
+  }
+]);
